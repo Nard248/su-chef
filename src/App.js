f56f8e164577237334acea3e0e60642fe3c187ff
Narek <meloyann87@gmail.com>
2024-08-18 20:03:00 +0400

    Catalogue and Mock Pages are written

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Catalogue from './pages/Catalogue';
+import Blog from './pages/Blog';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Footer from './components/Footer';
+import './styles/animations.css';
+
+function AnimatedRoutes() {
+    const location = useLocation();
+
+    return (
+        <TransitionGroup>
+            <CSSTransition
+                key={location.key}
+                classNames="page"
+                timeout={300}
+            >
+                <Routes location={location}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/catalogue" element={<Catalogue />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </CSSTransition>
+        </TransitionGroup>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <AnimatedRoutes />
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
